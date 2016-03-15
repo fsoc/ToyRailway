c81373e5373e76169fdc0fa374718c5d03dfb539
fsoc <fsoc@null>
2016-03-15 10:47:48 +0000

    Scanning input into Strings and ints.

--- src/main/java/fsoc/ToyRailway.java
+++ src/main/java/fsoc/ToyRailway.java
@@ -1,8 +1,27 @@
 /**
  * https://spotify.kattis.com/problems/railway
  */
+
 package fsoc;
+import java.util.Scanner;
+
 public class ToyRailway  {
-	public static void main(String[] args) {
-	}
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in).useDelimiter("\\s");
+
+    int switches = scanner.nextInt();
+    int connections = scanner.nextInt();
+
+    while (scanner.hasNext()) {
+      String s1 = scanner.next();
+      int connection1 = Integer.parseInt(s1.substring(0, s1.length() - 1));
+      String track1 = s1.substring(s1.length() - 1);
+      String s2 = scanner.next();
+      int connection2 = Integer.parseInt(s2.substring(0, s2.length() - 1));
+      String track2 = s2.substring(s2.length() - 1);
+
+    }
+
+    scanner.close();
+  }
 }
