d503c8df3339b6279ae7a8c678845dcd5071419e
fsoc <fsoc@altai.se>
2016-03-16 14:34:56 +0100

    Re-indented code.

--- src/main/java/fsoc/Point.java
+++ src/main/java/fsoc/Point.java
@@ -3,23 +3,23 @@ package fsoc;
 public class Point {
   private int trainSwitch;
   private Gate gate;
-  
+
   public Point(int trainSwitch, Gate gate) {
     this.trainSwitch = trainSwitch;
     this.gate = gate;
   }
-  
+
   public Gate getGate() {
     return gate;
   }
-  
+
   public int getSwitch() {
     return trainSwitch;
   }
-  
+
   public boolean equals(Point p) {
     if(trainSwitch == p.trainSwitch &&
-       gate == p.gate) {
+        gate == p.gate) {
       return true;
     } else {
       return false;
